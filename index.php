56a4392e95c42e094b0549ca51210ca4fb316990
Brendan Murphy <bjohnmurphy1984@gmail.com>
2015-10-23 20:01:56 -0400

    Updted styles

--- index.php
+++ index.php
@@ -1,62 +1,80 @@
-
 <!DOCTYPE html>
 <html>
-<head>
-
-	<title>Brendan Murphy P1 - DWA15 Portfolio</title>
-	<meta charset="utf-8">
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' rel='stylesheet'>
-    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' rel='stylesheet'>
-    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/spacelab/bootstrap.min.css" rel="stylesheet" integrity="sha256-j7Dtnd7ZjexEiPNbscbopFn9+Cs0b3TLipKsWAPHZIM= sha512-RFhfi6P8zWMAJrEGU+CPjuxPh3r/UUBGqQ+/o6WKPIVZmQqeOipGotH2ihRULuQ8wsMBoK15TSZqc/7VYWyuIw==" crossorigin="anonymous">
-	<link rel='stylesheet' href='style.css' type='text/css'>
-
-	
-</head>
-<body>
-
-
-	<div class="container">
-	
-		<h1>Brendan Murphy</h1> 
-		<h1>Dynamic Web Apps Portfolio</h1>
-		
-		<img src='headshot.jpg' alt='Brendan Murphy'>
-	</div>
-	<div class="container">		
-		<h3>XKCD Password Generator</h3>
-		View: <a href = "http://p2.brendanmurph.com">[link]</a>
-		Github: <a target="_blank" href="https://github.com/BrendanJohn/p2" title="Github"><i class="fa fa-2x fa-github"></i></a>
-		
-		<h3>Developer's Best Friend</h3>
-		View: <a href = "http://p3.brendanmurph.com">[link]</a>
-		Github: <a target="_blank" href="https://github.com/BrendanJohn/p3" title="Github"><i class="fa fa-2x fa-github"></i></a>
-		
-		<h3>Project 4 Title</h3>
-		View: [link]
-		Github: [link]
-
-
-		<h2>In 2+ sentences describe who you are and what you do.</h2>
-		<p>I am a Boston native (South Shore), my Grandmother emigrated from Ireland in the early 1900s. I have the freckles to prove it. I have lived in New England my whole life and went to college in New Hampshire. I was on the ski team and still ski regularly. We had 20 feet of snow last year so that worked out well. I also like to travel, and am currently a Web UI developer at World Travel Holdings.</p>
-		
-		<h2>Why are you taking this course?</h2>
-		<p>I am taking this course to improve my back end development skills. My primary skills reside on the front end where I work in HTML, CSS and JavaScript.</p>
-		
-		<h2>What previous experience do you have with programming?</h2>
-		<p>Basic to intermediate knowledge of programming. Primarily learned while earning a Computer Science cert at Quincy College, followed by 3 years of professional experience. Also lots and lots of trial and error.</p>
-		
-		<h2>Are you working on a Mac, PC, or some other platform?</h2>
-		<p>Windows 7 home build.</p>
-		
-		<h2>Anything else we should know?</h2>
-		<p>In Belichik we trust.</p>
-	</div>
-	
-	
-	
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-	
-</body>
+   <head>
+      <title>Brendan Murphy P1 - DWA15 Portfolio</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' rel='stylesheet'>
+      <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' rel='stylesheet'>
+      <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/spacelab/bootstrap.min.css" rel="stylesheet" integrity="sha256-j7Dtnd7ZjexEiPNbscbopFn9+Cs0b3TLipKsWAPHZIM= sha512-RFhfi6P8zWMAJrEGU+CPjuxPh3r/UUBGqQ+/o6WKPIVZmQqeOipGotH2ihRULuQ8wsMBoK15TSZqc/7VYWyuIw==" crossorigin="anonymous">
+      <link rel='stylesheet' href='style.css' type='text/css'>
+   </head>
+   <body>
+      <div class="container">
+         <h1>Brendan Murphy</h1>
+         <h1>Dynamic Web Apps Portfolio</h1>
+         <img src='headshot.jpg' alt='Brendan Murphy'>
+      </div>
+      <div data-sr class="container">
+         <div class="row">
+            <div class="col-lg-12 text-center">
+               <h2 class="h2">PROJECTS</h2>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-lg-4 col-lg-offset-2">
+               <h4>XKCD Password Generator</h4>
+               View: <a href = "http://p2.brendanmurph.com">p2.brendanmurph.com</a>
+            </div>
+            <div class="col-lg-4 col-lg-offset-2">
+               <strong>October 2015</strong>
+            </div>
+            <div class="col-lg-8 col-lg-offset-2">
+               <p>Github: <a target="_blank" href="https://github.com/BrendanJohn/p2" title="Github"><i class="fa fa-2x fa-github"></i></a></p>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-lg-4 col-lg-offset-2">
+               <h4>Developer's Best Friend</h4>
+               View: <a href = "http://p3.brendanmurph.com">p3.brendanmurph.com</a>
+            </div>
+            <div class="col-lg-4 col-lg-offset-2">
+               <strong>October 2015</strong>
+            </div>
+            <div class="col-lg-8 col-lg-offset-2">
+               <p>Github: <a target="_blank" href="https://github.com/BrendanJohn/p3" title="Github"><i class="fa fa-2x fa-github"></i></a></p>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-lg-4 col-lg-offset-2">
+               <h4>Project 4</h4>
+               View: <a href = "http://p2.brendanmurph.com"></a>
+            </div>
+            <div class="col-lg-4 col-lg-offset-2">
+               <strong>2015</strong>
+            </div>
+            <div class="col-lg-8 col-lg-offset-2">
+               <p>Github: <a target="_blank" href="https://github.com/BrendanJohn/p4" title="Github"><i class="fa fa-2x fa-github"></i></a></p>
+            </div>
+         </div>
+      </div>
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+               <hr>
+               <h3>In 2+ sentences describe who you are and what you do.</h3>
+               <p>I am a Boston native (South Shore), my Grandmother emigrated from Ireland in the early 1900s. I have the freckles to prove it. I have lived in New England my whole life and went to college in New Hampshire. I was on the ski team and still ski regularly. We had 20 feet of snow last year so that worked out well. I also like to travel, and am currently a Web UI developer at World Travel Holdings.</p>
+               <h3>Why are you taking this course?</h3>
+               <p>I am taking this course to improve my back end development skills. My primary skills reside on the front end where I work in HTML, CSS and JavaScript.</p>
+               <h3>What previous experience do you have with programming?</h3>
+               <p>Basic to intermediate knowledge of programming. Primarily learned while earning a Computer Science cert at Quincy College, followed by 3 years of professional experience. Also lots and lots of trial and error.</p>
+               <h3>Are you working on a Mac, PC, or some other platform?</h3>
+               <p>Windows 7 home build.</p>
+               <h3>Anything else we should know?</h3>
+               <p>In Belichik we trust.</p>
+            </div>
+         </div>
+      </div>
+      <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+   </body>
 </html>
